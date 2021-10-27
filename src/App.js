93d2b9357606465622100695d2cd1b075397eaa6
venkateshatgit <vpgunake_b18@me.vjti.ac.in>
2021-10-27 22:03:05 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Headers from './components/Headers'
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Headers/>
+      <div className="balance-container">
+        <Balance/>
+        <IncomeExpenses/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Headers.jsx
@@ -0,0 +1,9 @@
+function Headers() {
+    return (  
+        <div>
+            <h1>Header Component</h1>
+        </div>
+    );
+}
+
+export default Headers;
